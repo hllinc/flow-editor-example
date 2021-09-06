2aca07cc3384c8639394c3e0e135a5ec1803e704
hllinc <hllinc@163.com>
2021-09-06 16:07:23 +0800

    依赖路径修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import FlowEditor from '@hongliang-org/flow-editor/dist/src';
+import FlowEditor from '@hongliang-org/flow-editor';
 
 const flowMockData = {
     height: 800,
